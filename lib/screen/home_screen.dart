686e84dbcdbf7e5eecaaf746833d5d8efa1c2b91
tmbb-japan <woo787@naver.com>
2023-07-21 10:10:08 +0900

    feat: family&autoDispose modifier

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:theory_rvp/layout/default_layout.dart';
+import 'package:theory_rvp/screen/family_modifier_screen.dart';
 import 'package:theory_rvp/screen/future_provider_screen.dart';
 import 'package:theory_rvp/screen/state_notifier_provider_screen.dart';
 import 'package:theory_rvp/screen/state_provider_screen.dart';
 import 'package:theory_rvp/screen/stream_provider_screen.dart';
+
+import 'auto_dispose_modifier_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -53,6 +56,26 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
           ),
         ],
       ),
