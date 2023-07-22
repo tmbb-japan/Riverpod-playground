bb78aa7f60c13342bfaf94dce78b0848e5819bf8
tmbb-japan <woo787@naver.com>
2023-07-22 17:31:12 +0900

    feat : listen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:theory_rvp/layout/default_layout.dart';
 import 'package:theory_rvp/screen/family_modifier_screen.dart';
 import 'package:theory_rvp/screen/future_provider_screen.dart';
+import 'package:theory_rvp/screen/listen_provider_screen.dart';
 import 'package:theory_rvp/screen/state_notifier_provider_screen.dart';
 import 'package:theory_rvp/screen/state_provider_screen.dart';
 import 'package:theory_rvp/screen/stream_provider_screen.dart';
@@ -76,6 +77,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),
