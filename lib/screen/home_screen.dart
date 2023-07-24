0c3ebfde9a6c2da7654b9ba5c91863ba2753b024
tmbb-japan <woo787@naver.com>
2023-07-24 12:29:41 +0900

    feat : codeGeneration for riverPod

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:theory_rvp/layout/default_layout.dart';
+import 'package:theory_rvp/screen/code_generation_screen.dart';
 import 'package:theory_rvp/screen/family_modifier_screen.dart';
 import 'package:theory_rvp/screen/future_provider_screen.dart';
 import 'package:theory_rvp/screen/listen_provider_screen.dart';
@@ -109,6 +110,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('ProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => CodeGenerationScreen(),
+                ),
+              );
+            },
+            child: Text('CodeGenerationScreen'),
           ),
         ],
       ),
