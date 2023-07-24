4d5113bd05c60fa9670adada9ec4f89abb4ea54c
tmbb-japan <woo787@naver.com>
2023-07-24 12:07:57 +0900

    feat : observer & version up for codeGenerating

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:theory_rvp/riverpod/provider_observer.dart';
 import 'package:theory_rvp/screen/home_screen.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [
+        Logger(),
+      ],
       child: MaterialApp(
         home: HomeScreen(),
       ),
